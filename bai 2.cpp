d5d2f9157624649f7a78178c317b537978ada5ae
QTruongaccount <ngquagtruog0605@gmail.com>
2024-10-12 19:56:26 +0700

    bai 2.cpp

--- bai 2.cpp
+++ bai 2.cpp
@@ -1,46 +1,55 @@
-#include<iostream>
-#include<algorithm>
+#include <iostream>
+#include <algorithm>
 using namespace std;
-void Sapxepdiem(double *diem,int size){
-	sort (diem, diem + size);
+
+void Sapxepdiem(double *diem, int size) {
+    sort(diem, diem + size);
 }
-double Diemtrungbinh(double *diem, int size)
-{
-	double S=0;
-	int i=0;
-	for(i=0;i<size;i++){
-		S+=*(diem+i);
-		
-	}
-	return S/size;
+
+double Diemtrungbinh(double *diem, int size) {
+    double S = 0;
+    for (int i = 0; i < size; i++) {
+        S += *(diem + i);
+    }
+    return S / size;
 }
-int main()
-{
-	int Soluongdiem;
-	cout<<"Nhap so luong diem kiem tra: "; 
-	cin>>Soluongdiem;
-	if(Soluongdiem<=0){
-		cout<<"So luong diem phai lon hon 0"<<endl;
-		return 1;
-	}
-	double *diem=new double [Soluongdiem];
-	for (int i=0;i<Soluongdiem;++i){
-		do{
-			cout<<"Nhap diem kiem tra thu "<< i+1 <<":";
-			cin>> diem[i]<0;
-			if (diem[i]<0){
-				cout<<"Diem khong duoc la so am. Vui long nhap lai "<<endl;
-			}
-		}
-		while(*(diem+i)<0);
-	}
-	Sapxepdiem(diem,Soluongdiem);
-	double a=Diemtrungbinh(diem,Soluongdiem);
-	cout<<"\nDanh sach diem da sap xep: \n";
-	for (int i = 0; i <Soluongdiem; ++i)
-		{
-			cout<< diem [i]<< " ";
-		}
-	cout <<"\nDiem trung binh: "<<a<<endl; 
-	delete[] diem; 
+
+int main() {
+    int Soluongdiem;
+    cout << "Nhap so luong diem kiem tra: ";
+    cin >> Soluongdiem;
+
+    if (Soluongdiem <= 0) {
+        cout << "So luong diem phai lon hon 0" << endl;
+        return 1;
+    }
+
+    double *diem = new(nothrow) double[Soluongdiem]; 
+    if (!diem) {
+        cout << "Khong the cap phat bo nho!" << endl;
+        return 1;
+    }
+
+    for (int i = 0; i < Soluongdiem; ++i) {
+        do {
+            cout << "Nhap diem kiem tra thu " << i + 1 << ": ";
+            cin >> diem[i];
+            if (diem[i] < 0) {
+                cout << "Diem khong duoc la so am. Vui long nhap lai." << endl;
+            }
+        } while (diem[i] < 0);
+    }
+
+    Sapxepdiem(diem, Soluongdiem);
+    double a = Diemtrungbinh(diem, Soluongdiem);
+    
+    cout << "\nDanh sach diem da sap xep: \n";
+    for (int i = 0; i < Soluongdiem; ++i) {
+        cout << diem[i] << " ";
+    }
+    
+    cout << "\nDiem trung binh: " << a << endl;
+    
+    delete[] diem;
+    return 0; 
 }
